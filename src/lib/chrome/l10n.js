c68162fbe7c56a07c1d6ecd2eed0cc2ac48bea3a
token <token@positrium.org>
2019-03-28 03:13:49 +0900

    fix ddd chrome ext env

---
+++ src/lib/chrome/l10n.js
@@ -0,0 +1,17 @@
+// https://developer.chrome.com/extensions/i18n
+function localizeHtmlPage() {
+    document.querySelectorAll("[data-i18n-text]").forEach((element) => {
+        const key = element.getAttribute("data-i18n-text");
+        element.textContent = chrome.i18n.getMessage(key);
+    });
+    document.querySelectorAll("[data-i18n-value]").forEach((element) => {
+        if (element instanceof HTMLInputElement) {
+            const key = element.getAttribute("data-i18n-value");
+            element.value = chrome.i18n.getMessage(key);
+        }
+        else {
+            Logger.error("[ERROR] element is not HTMLInputElement!", element);
+        }
+    });
+}
+localizeHtmlPage();
